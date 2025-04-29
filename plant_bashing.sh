81d03a3098d5797d307589cf9ec855664654a9d8
ualuy <chesterachit@gmail.com>
2025-04-29 15:27:51 -0500

    Milestone 2 started

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -1,9 +1,42 @@
 #!bin/bash
+echo "Hello new user! What's your name?"
+read username
+echo "Welcome, $username"
+sleep 1
+echo "Do you want to plant a new seed?"
+sleep 1
+echo "Type y to plant and q to quit"
 
-echo "hello new user!"
-read $username
-echo "Welcome, $username."
+while true; do
+	read answer1
+	if [ "$answer1" == "y" || "$answer1" == "Y" ]; then
+		echo "You dug a hole, and planted a small grain-sized seed."
+		sleep 2
+		echo "This tiny world you are spectating flies way faster than your world does. Everything passes quicker in here."
+		sleep 2
+		echo "Do you want to sit by and wait for the seed to grow?" 
+		sleep 1
+		echo "Type y to wait and q to quit."
+		while true; do
+			read answer2
+			if [ "$answer2" == "y" || "$answer1" == "Y" ] then
+				echo "The seed will always take 3 days to grow."
+				echo "You waited for a whole day, but nothing happened."
+				line=
+			for (( i=0; i<${#line}; i++ )); do
+				printf "${line:$i:1}"
+				sleep 0.1
+			done
 
-sleep 5
-
-exit
+	elif [ "$answer1" == "q" || "$answer1" == "Q" ]; then 
+		line="The world collapses to a singularity as you fade out..."
+	for (( i=0; i<${#line}; i++ )); do
+		printf "${line:$i:1}"
+		sleep 0.1
+	done
+	sleep 2
+	exit
+	else
+	echo "Invalid answer. Please reenter."
+fi
+done
