3b52409887d4a8ac7dd78f3512b7c04aac30e1d3
ualuy <chesterachit@gmail.com>
2025-05-01 15:31:35 -0500

    Milestone 2 complete

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -1,31 +1,68 @@
 #!bin/bash
 
 dayn=0
-dayr1=3
-dayr2=6
 msgwait="You waited for a whole day, but nothing happened."
+plheight=0
+plleaves=0
 
 wait() {
 	echo "Do you want to wait even more?(y/n)"
-	read ans
-	if [[ $ans == "y" || $ans == "Y" ]]; then
-		((dayn++))
-	elif [[ $ans == "n" || $ans == "N" ]]; then
-		test
-	fi
-}
-
-
-test() {
-	line="The world collapses to a singularity as you fade out..."
-	for (( i=0; i<${#line}; i++ )); do
-		printf "${line:$i:1 }"
-		sleep 0.2
+	while true; do
+		read ans
+		if [[ $ans == "y" || $ans == "Y" ]]; then
+			((dayn++))
+			echo "This is day $dayn."
+			if [[ $dayn -lt 3 ]]; then
+				echo "Nothing has happened yet."
+				wait
+			elif [[ $dayn == 3 ]]; then
+				echo "Your seed germinated overnight!"
+				wait
+			elif [[ $dayn -gt 3 ]] && [[ $dayn -lt 6 ]]; then
+				echo "Nothing happened yet..."
+				wait
+			elif [[ $dayn == 6 ]]; then
+				echo "Your seed has grown into a green little plant!"
+				wait	
+			elif [[ $dayn -gt 6 ]] && [[ $dayn -lt 21 ]];then
+				echo "Your plant has grown 2 cm!"
+				sleep 1
+				echo "And has got two leaves!"
+				((plheight += 2))
+				((plleaves += 2))
+				sleep 1
+				echo "Day count: $dayn"
+				sleep 1
+				echo "Your plant's current height: $plheight"
+				sleep 1
+				echo "Your plant's current leave count: $plleaves"
+				wait
+			else 
+				echo "Your plant has reached the end of its lifespan."
+				sleep 1
+				echo "Day count: $dayn"
+				sleep 1
+				echo "Your plant's current height: $plheight"
+				sleep 1
+				echo "Your plant's current leave count: $plleaves"
+				sleep 1
+				echo "It has lived a happy life."
+				sleep 1
+				echo "Goodbye, $username!"
+				sleep 1
+				exit
+			fi
+		elif [[ $ans == "n" || $ans == "N" ]]; then
+			echo "Goodbye $username!"
+			sleep 0.5
+			echo "It's been a great time knowing you."
+			sleep 0.5
+			exit
+		else 
+			echo "Invalid response. Please reenter."
+		fi
 	done
-	exit
 }
-
-
 
 echo "Hello new user! What's your name?"
 read username
@@ -33,29 +70,28 @@ echo "Welcome, $username"
 sleep 1
 echo "Do you want to plant a new seed?(y/n)"
 
-#if [[ $dayn -lt $dayr1 ]]; then  
 while true; do
-	read answer1
-	if [[ $answer1 == "y" || $answer1 == "Y" ]]; then
+	read ans
+	if [[ $ans == "y" || $ans == "Y" ]]; then
 		echo "You dug a hole, and planted a small grain-sized seed."
-		sleep 2
-		echo "This tiny world you are spectating flies way faster than your world does. Everything passes quicker in here."
-		sleep 2
+		sleep 1
+		echo "This tiny world you are spectating flies way faster"
+		echo "than your world does. Everything passes quicker in here."
+		sleep 1
 		echo "Do you want to sit by and wait for the seed to grow?(y/n)" 
 		while true; do
-			read answer2
+			read ans
 			((dayn++))
-			if [[ $answer2 == "y" || $answer1 == "Y" ]]; then
-				echo "The seed will always take 3 days to grow. $msgwait "
+			if [[ $ans == "y" || $ans == "Y" ]]; then
+				echo "The seed will always take 3 days to grow. "
+				echo "$msgwait"
 				echo "This is day $dayn. "
 				wait
 			fi
 		done
-	elif [[ $answer1 == "n" || $answer1 == "N" ]]; then 
+	elif [[ $ans == "n" || $ans == "N" ]]; then 
 		test
 	else
 		echo "Invalid answer. Please reenter."
 	fi
-#elif [[ $dayn -gt $dayr2 ]]; then
-
 done
