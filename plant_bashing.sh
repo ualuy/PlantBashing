0fd67546c76d4dbd979571cb5551c7d38a225125
ualuy <chesterachit@gmail.com>
2025-05-20 15:07:03 -0500

    Milestone5 comp

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -5,6 +5,9 @@ msgwait="You waited for a whole day, but nothing happened."
 plheight=0
 plleaves=0
 usrinput=false
+namecycle=0
+names=("Morpheus" "Analiea" "Izzy")
+
 
 # Start game
 play() {
@@ -38,23 +41,37 @@ gamecycle() {
             plant=false
             echo "What should its name be?"
             read plantname
+            names+=("$plantname")
             echo "Your plant's name will be $plantname!"
         elif [[ $ans == "n" || $ans == "N" ]]; then
             plant=false
-            echo "Since you didn't name it, its name shall be Morpheus."
+            echo "Since you didn't name it, its name shall be ${names[$namecycle]}."
+            if [[ $namecycle -lt 2 ]]; then
+                ((namecycle++))
+            else
+                namecycle=0
+            fi
+        elif [[ $ans == ";" ]]; then
+            plexit
+        elif [[ $ans == "-" ]]; then
+            usrinput=false
+            play
         else
             echo "Invalid response, please reenter."
         fi
     done
-    echo "If you ever want to exit, just hit the ; key."
     sleep 0.5
-    echo "Do you want to wait for $plantname to grow? (y/n)"
+    echo "If you ever want to exit, just wait for a question and input ';' " 
+    sleep 0.5
+    echo "or if you want to restart the game, just input '-' "
+    sleep 0.5
+    echo "Do you want to wait for $names to grow? (y/n)"
     playerans=true
     while [ "$playerans" = true ]; do
         read ans
         if [[ $ans == "y" || $ans == "Y" ]]; then
             playerans=false
-            echo "$plantname will take 3 days to germinate."
+            echo "$names will take 3 days to germinate."
             echo "You are waiting for the first day to pass..."
             sleep 3
             echo "The day has passed."
@@ -63,6 +80,11 @@ gamecycle() {
         elif [[ $ans == "n" || $ans == "N" ]]; then
             playerans=false
             plexit
+        elif [[ $ans == ";" ]]; then
+            plexit
+        elif [[ $ans == "-" ]]; then
+            usrinput=false
+            play
         else
             echo "Invalid response, please reenter."
         fi
@@ -71,42 +93,48 @@ gamecycle() {
 
 # Game logic
 ask() {
-    questions=("Wanna keep waiting for $plantname to grow?" 
-           "Do you want to wait for $plantname to grow?" 
-           "Keep waiting?")
+    questions=("Wanna keep waiting for $names to grow?" "Do you want to wait for $names to grow?" "Keep waiting?")
     size=${#questions[@]}
     rand_index=$((RANDOM % size))
+    weather=("Rainy" "Sunny" "Cloudy" "Overcast" "Windstorm" "Foggy" "Thunderstorm")
+    randompick=$((RANDOM % ${#weather[@]} ))
     echo "${questions[$rand_index]} (y/n)"
     startloop=true
     while [ "$startloop" = true ]; do
         read ans
         if [[ $ans == "y" || $ans == "Y" ]]; then
             ((dayn++))
-            sleep 2
+            sleep 1
             echo "This is day $dayn."
             if [[ $dayn -lt 3 ]]; then
                 echo "Nothing has happened yet..."
+                weathera
                 ask
             elif [[ $dayn == 3 ]]; then
-                echo "$plantname germinated overnight! 🎉"
+                echo "$names germinated overnight! 🎉"
+                weathera
                 ask
             elif [[ $dayn -gt 3 && $dayn -lt 6 ]]; then
                 echo "Nothing has happened yet..."
+                weathera
                 ask
             elif [[ $dayn == 6 ]]; then
-                echo "$plantname has grown into a green little plant! 🎉"
+                echo "$names has grown into a green little plant! 🎉"
+                weathera
                 ask
             elif [[ $dayn -gt 6 && $dayn -lt 21 ]]; then
-                echo "$plantname has grown 2 cm and sprouted two new leaves!"
+                echo "$names has grown 2 cm and sprouted two new leaves!"
                 ((plheight += 2))
                 ((plleaves += 2))
                 echo "Height: $plheight cm, Leaves: $plleaves"
+                weathera
                 ask
             else
-                echo "$plantname has reached the end of its lifespan."
+                weathera
+                echo "$names has reached the end of its lifespan."
                 echo "Final day count: $dayn"
                 echo "Height: $plheight cm, Leaves: $plleaves"
-                echo "$plantname lived a happy life."
+                echo "$names lived a happy life."
                 echo "Do you want to play again? (y/n)"
                 startloop=false
                 read ans
@@ -118,10 +146,19 @@ ask() {
             fi
         elif [[ $ans == "n" || $ans == "N" ]]; then
             plexit
+        elif [[ $ans == ";" ]]; then
+            plexit
+        elif [[ $ans == "-" ]]; then
+            usrinput=false
+            play
         else
             echo "Invalid response. Please reenter."
         fi
     done
+}
+
+weathera() {
+        echo "Today's weather is ${weather[$randompick]}."
 }
 
 # Exit game
