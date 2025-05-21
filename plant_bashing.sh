d718ffd7472547d7d1665274dfd9cf4a58a87707
ualuy <chesterachit@gmail.com>
2025-05-21 15:30:09 -0500

    Milstone 6 complete

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+survivecounter=0
 dayn=0
 msgwait="You waited for a whole day, but nothing happened."
 plheight=0
@@ -8,7 +9,28 @@ usrinput=false
 namecycle=0
 names=("Morpheus" "Analiea" "Izzy")
 growthrate=2
+growthmsgs=("$names has grown 1.5 cm, and $(bc <<< "2 + (2.5 * growthrate)") leaves!"
+            "$names grew 1.5 cm and sprouted $(bc <<< "2 + (2.5 * growthrate)") more leaves!" 
+            "$names seems very energetic today! Height +1.5, and leaves + $(bc <<< "2 + (2.5 * growthrate)").")
 
+diemsg() {
+    echo "Final day count: $dayn"
+    echo "Height: $plheight cm, Leaves: $plleaves"
+    echo "Windstorms survived: $survivecounter."
+    echo "$names lived a happy life."
+    echo "Do you want to play again? (y/n)"
+}
+
+growecho() {
+    echo "${growthmsgs[$msgpick]}"
+    echo "Current height: $plheight, current leaf count: $plleaves"
+}
+
+limit0() {
+    if [[ $growthrate -lt 0 ]]; then
+        growthrate=0
+    fi
+}
 
 # Start game
 play() {
@@ -74,7 +96,7 @@ gamecycle() {
             playerans=false
             echo "$names will take 3 days to germinate."
             echo "You are waiting for the first day to pass..."
-            sleep 3
+            sleep 2
             echo "The day has passed."
             echo "Nothing has happened yet..."
             ask
@@ -118,30 +140,35 @@ ask() {
                 ask
             elif [[ $dayn == 6 ]]; then
                 echo "$names has sprouted and turned into a green little plant! 🎉"
+                echo "Its current height is 2 cm."
+                plheight=2
                 ask
-            elif [[ $dayn -gt 6 && $dayn -lt 21 ]]; then
-                echo "$names has grown 2 cm and sprouted two new leaves!"
-                ((plheight += 2))
-                ((plleaves += 2))
-                echo "Height: $plheight cm, Leaves: $plleaves"
+            elif [[ $dayn -gt 6 ]]; then
                 weathera
-                ask
-            else
-                weathera
-                echo "$names has reached the end of its lifespan."
-                echo "Final day count: $dayn"
-                echo "Height: $plheight cm, Leaves: $plleaves"
-                echo "$names lived a happy life."
-                echo "Do you want to play again? (y/n)"
-                startloop=false
-                read ans
-                if [[ $ans == "y" || $ans == "Y" ]]; then
-                    play
-                else
-                    plexit
+                if [[ $plheight -gt 35 ]]; then
+                    echo "$names has exceeded the height of 35 cm, and is truly independent from you!"
+                    echo "It will take care of itself now."
+                    diemsg
+                    startloop=false
+                    waitforans = true
+                        while [ "$waitforans" = true ]; do 
+                        read ans
+                        if [[ $ans == "y" || $ans == "Y" ]]; then
+                            waitforans=false
+                            play
+                        elif [[ $ans == "n" || $ans == "N" ]]; then
+                            waitforans=false
+                            plexit
+                        else
+                            echo "Invalid response. Answer again, please."
+                        fi
+                    done
+                    retun
                 fi
+                ask
             fi
         elif [[ $ans == "n" || $ans == "N" ]]; then
+            diemsg
             plexit
         elif [[ $ans == ";" ]]; then
             plexit
@@ -155,9 +182,40 @@ ask() {
 }
 
 weathera() {
-        echo "Today's weather is ${weather[$randompick]}."
-        if [[ $randompick == "sunny" ]]; then
-
+    msgpick=$((RANDOM % ${#growthmsgs[@]} ))
+    weathertoday=${weather[$randompick]}
+    echo "Today's weather is $weathertoday."
+    if [[ $weathertoday == "sunny" ]]; then
+        ((growthrate += 3))
+        growth
+        growecho
+    elif [[ $weathertoday == "rainy" ]]; then
+        ((growthrate += 2))
+        growecho
+    elif [[ $weathertoday == "cloudy" ]]; then
+        echo "No growth today..."
+    elif [[ $weathertoday == "foggy" ]]; then
+        ((growthrate -= 1))
+        limit0
+        echo "$names seems to be a little down today..."
+    elif [[ $weathertoday == "overcast" ]]; then
+        growth 
+        growecho
+    elif [[ $weathertoday == "windstorm" ]]; then
+        ((growthrate -= 2))
+        ((survivecounter += 1))
+        limit0
+    elif [[ $weathertoday == "thunderstorm" ]]; then
+        echo "There was a thunderstorm and $names did not survive."
+        diemsg
+            startloop=false
+                read ans
+                if [[ $ans == "y" || $ans == "Y" ]]; then
+                    play
+                else
+                    plexit
+                fi
+    fi
 }
 
 growth() {
